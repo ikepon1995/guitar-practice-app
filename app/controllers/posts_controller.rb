b3688ec538acf0c6fc3c0f96399a86b62808826d
ikepon <ikepon052707@gmail.com>
2025-05-30 07:37:38 +0900

    投稿機能の作成

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
+  def index
+    @posts = Post.all.includes(:user).order(created_at: :desc)
+  end
+
+  def show
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to @post, notice: "投稿が作成されました！"
+    else
+      render :new, status: :unprocessable_entity
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: "投稿が更新されました！"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_path, notice: "投稿が削除されました。"
+  end
+
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def authorize_user!
+    redirect_to posts_path, alert: "権限がありません。" unless @post.user == current_user
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+end
